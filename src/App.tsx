68c18b26951e675fb9e7e3d047904a12b21f90c5
Bhushan Shah <bhushan.shah.dev@gmail.com>
2021-07-21 00:48:41 -0700

    Added a basic stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+enum TimerState {
+  STARTED,
+  PAUSED,
+  STOPPED
+}
+
 function App() {
+  const [time, setTime] = useState(new Date());
+  const [centiseconds, setCentiseconds] = useState(0);
+
+  const [timerState, setTimerState] = useState<TimerState>(TimerState.STOPPED);
+
+  // Start an interval to handle current time update
+  useEffect(() => {
+    const interval = window.setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
+  // Event handlers
+  const startTimer = () => {
+    setTimerState(TimerState.STARTED);
+  };
+
+  const pauseTimer = () => {
+    setTimerState(TimerState.PAUSED);
+  };
+
+  const stopTimer = () => {
+    setTimerState(TimerState.STOPPED);
+  };
+
+  // Handle timer state change
+  useEffect(() => {
+    let interval: number;
+    if (timerState === TimerState.STARTED) {
+      interval = window.setInterval(() => {
+        setCentiseconds(s => s+1);
+      }, 10);
+    }
+    if (timerState === TimerState.PAUSED) {
+      /* @ts-ignore */
+      if (interval) {
+        clearInterval(interval);
+      }
+    }
+    if (timerState === TimerState.STOPPED) {
+      /* @ts-ignore */
+      if (interval) {
+        clearInterval(interval);
+      }
+      setCentiseconds(0);
+    }
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    }
+  }, [timerState]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>React Stopwatch</h3>
+        <h4>{`The current time is ${time.toLocaleTimeString()}`}</h4>
+        <h1>{`${Math.floor(centiseconds/100).toString()}.${(centiseconds % 100).toString().padStart(2, '0')} seconds`}</h1>
+        <div className="controls">
+          <button onClick={startTimer}>Start</button>
+          <button onClick={pauseTimer}>Pause</button>
+          <button onClick={stopTimer}>Stop</button>
+        </div>
       </header>
     </div>
   );
